b2b20c2f9431d5561e4a1c720b86d806eda80d4b
nichthai <57887348+nichthai@users.noreply.github.com>
2020-09-01 19:24:32 -0500

    commit

--- meat-cheese-galactic-2/Properties/AssemblyInfo.cs
+++ meat-cheese-galactic-2/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Meat&Cheese Galactic 2")]
+[assembly: AssemblyTitle("Meat&Cheese Galactic")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
