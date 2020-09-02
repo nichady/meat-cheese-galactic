67d70fdc6c978de25f88162580dd23d31f8a5736
nichthai <57887348+nichthai@users.noreply.github.com>
2020-09-01 19:33:23 -0500

    commit

--- server/Properties/AssemblyInfo.cs
+++ server/Properties/AssemblyInfo.cs
@@ -3,7 +3,7 @@ using System.Reflection;
 // Information about this assembly is defined by the following attributes.
 // Change them to the values specific to your project.
 
-[assembly: AssemblyTitle("Meat&Cheese Galactic 2 Server")]
+[assembly: AssemblyTitle("Meat&Cheese Galactic Server")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
